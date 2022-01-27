45acf269a684ad2325555365da6cea65bfdc9a37
hayattofik <96552730+hayattofik@users.noreply.github.com>
2022-01-27 10:59:56 +0300

    Delete src directory

--- src/main/java/com/hagere/hager/postrepository.java
+++
@@ -1,7 +0,0 @@
-package com.hagere.hager;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface postrepository extends CrudRepository<post, Long> {
-}
